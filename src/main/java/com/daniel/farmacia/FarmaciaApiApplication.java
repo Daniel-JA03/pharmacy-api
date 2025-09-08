591091a00f4e0920a2aef3c5c6adfd667ed70bcc
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-09-08 11:48:50 -0500

    first commit

---
+++ src/main/java/com/daniel/farmacia/FarmaciaApiApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaApiApplication.class, args);
+	}
+
+}
